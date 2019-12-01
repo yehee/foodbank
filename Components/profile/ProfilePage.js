36af6cfb5a482cb00ae71c2822675c6c002ccfd4
Alice <alice.kim@concur.com>
2019-11-30 19:29:33 -0800

    done

--- Components/profile/ProfilePage.js
+++ Components/profile/ProfilePage.js
@@ -22,7 +22,7 @@ export default class ProfilePage extends Component {
                     <Text style={styles.header}>Profile</Text>
                 </View>
                 <View style={styles.view}>
-                    <Image style={styles.profile} source={require('../../assets/profile-big.png')} />
+                    <Image style={{ ...styles.profile, width: 100, height: 100, resizeMode: 'contain' }} source={require('../../assets/profile-big.png')} />
                     <View>
                         <Text style={styles.title}>John Smith</Text>
                         <Text style={styles.info}>Line Cook</Text>
@@ -31,12 +31,12 @@ export default class ProfilePage extends Component {
                 </View>
                 <View style={styles.view2}>
                     <View>
-                        <Text style={styles.subTitles}>YOUR FOOD BANK</Text>
+                        <Text style={{ ...styles.subTitles, color: '#008391', marginBottom: 10 }}>YOUR FOOD BANK</Text>
                         <Text>Phone: 604-876-3601</Text>
                         <Text>1150 Raymur Avenue</Text>
                         <Text>Vancouver, Canada</Text>
                     </View>
-                    <Image style={styles.logo} source={require('../../assets/profile-big.png')} />
+                    <Image style={{ ...styles.logo, width: 175, resizeMode: 'contain', height: 100 }} source={require('../../assets/FoodBank_Transparent_Blue.Logo.png')} />
                 </View>
                 <View style={styles.buttonView}>
 
@@ -65,6 +65,7 @@ export default class ProfilePage extends Component {
                 >
                     <Icon name='home' type='material' onPress={() => navigate('Dashboard')} />
                     <Icon name='price-ribbon' type='entypo' onPress={() => navigate('Leaderboard')} />
+                    <Icon name='sheriff-badge' type='foundation' onPress={() => navigate('Achievements')} />
                     <Icon name='user' type='feather' onPress={() => navigate('Profile')} />
                 </View>
             </View>
@@ -96,19 +97,16 @@ const styles = StyleSheet.create({
         width: 60,
     },
     title: {
-        fontSize: 25,
+        fontSize: 18,
         fontWeight: 'bold',
         marginLeft: 25,
         marginTop: 15,
-
-
     },
     profile: {
         paddingRight: 0,
         marginRight: 0,
         marginTop: 5,
         marginLeft: 30,
-
         width: 130,
         height: 130,
     },
@@ -118,19 +116,19 @@ const styles = StyleSheet.create({
     header: {
         marginTop: 80,
         marginLeft: 30,
-        fontSize: 50,
+        fontSize: 28,
         fontWeight: 'bold',
         color: '#008391',
     },
     info: {
         marginLeft: 27,
-
     },
 
     subTitles: {
-
         fontSize: 20,
         fontWeight: 'bold',
+        color: '#008391',
+        marginBottom: 10
     },
 
     view2: {
